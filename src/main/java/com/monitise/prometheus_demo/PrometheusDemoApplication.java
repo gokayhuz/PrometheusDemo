b3edd456551c54322c4e02784b210111fbd5c567
gokay huz <gokay.huz@monitise.com>
2017-08-09 10:47:54 +0300

    added prometheus annotations

--- src/main/java/com/monitise/prometheus_demo/PrometheusDemoApplication.java
+++ src/main/java/com/monitise/prometheus_demo/PrometheusDemoApplication.java
@@ -1,9 +1,13 @@
 package com.monitise.prometheus_demo;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint // sets up the prometheus endpoint /prometheus-metrics
+@EnableSpringBootMetricsCollector // exports the data at /metrics at a prometheus endpoint
 public class PrometheusDemoApplication {
 
 	public static void main(String[] args) {
